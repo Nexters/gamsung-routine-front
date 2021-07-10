a99583efc6584bcedc2438f443e9b9202716dbd2
Yuni-Q <lyh6425@gmail.com>
2021-07-11 02:12:47 +0900

    feat: 메인 레이아웃 기촌 작업
    fixed: #7

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import {Button, SafeAreaView, Text} from 'react-native';
+import {Button, SafeAreaView, Text, View} from 'react-native';
 import {NavigationProp} from '../../App';
-import styled from "@emotion/native";
+import styled from '@emotion/native';
 
 const EmptyText = styled.Text`
-    font-weight: bold;
-    font-size: 20px;
-`
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+`;
 
 const EmptyImage = styled.Image`
-    flex-direction: column;    
-`
+  width: 80px;
+  height: 80px;
+  margin: 0 auto 48px;
+`;
 
 export const Home = ({navigation}: NavigationProp) => {
   return (
-    <SafeAreaView
-      style={{flex: 1}}>
-        <Text>내 하루 테스크 0</Text>
-        <EmptyImage source={require("../assets/empty_monster.png")}/>
+    <SafeAreaView style={{flex: 1, justifyContent: 'space-between'}}>
+      <Text>내 하루 테스크 0</Text>
+      <View style={{justifyContent: 'center', alignContent: 'center'}}>
+        <EmptyImage source={require('../assets/empty_monster.png')} />
         <EmptyText>루틴이 없어요.</EmptyText>
         <EmptyText>루틴을 추가해요.</EmptyText>
-        <Button title="+" onPress={() => navigation.navigate('AddTask')} />
+      </View>
+      <Button title="+" onPress={() => navigation.navigate('AddTask')} />
     </SafeAreaView>
   );
 };
