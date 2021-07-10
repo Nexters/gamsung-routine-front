cc84f03cdd1f76dff4ff8b75507b0ee2e20e351e
Yuni-Q <lyh6425@gmail.com>
2021-07-10 23:02:16 +0900

    feat: 해커톤을 위한 레이아웃 구성
    fixed: #1

--- App.tsx
+++ App.tsx
@@ -1,52 +1,104 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+import BottomSheet from 'reanimated-bottom-sheet';
+import Modal from 'react-native-modal';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home3"
-            component={Home}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="AddTask" component={AddTask} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AddTask" component={AddTask} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
 
-const Home = () => {
+type RootStackParamList = {
+  Home: undefined;
+  AddTask: undefined;
+};
+
+interface Prop {
+  navigation: StackNavigationProp<RootStackParamList>;
+}
+
+function Home({navigation}: Prop) {
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
   return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="+" onPress={() => navigation.navigate('AddTask')} />
+      <Button title="Show modal" onPress={toggleModal} />
+
+      <Modal
+        isVisible={isModalVisible}
+        style={{
+          width: '100%',
+          margin: 0,
+          backgroundColor: 'rgba(25,25,25,0.8)',
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+        backdropOpacity={0}
+        hideModalContentWhileAnimating={true}
+        useNativeDriver={true}>
+        <View>
+          <Button title="Close modal" onPress={toggleModal} />
+          <Text style={{color: 'white', textAlign: 'center'}}>111</Text>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
+}
+const AddTask = ({navigation}: Prop) => {
+  const sheetRef = React.useRef(null);
+
+  const renderContent = () => (
     <View
-      style={{flex: 1, width: '100%', height: '100%', backgroundColor: 'red'}}>
-      <Text>!!!Home1</Text>
-      <Text>!!!Home1</Text>
-      <Text>!!!Home1</Text>
-      <Text>!!!Home1</Text>
-      <Text>!!!Home1</Text>
-      <Text>!!!Home1</Text>
-      <TouchableOpacity>
-        <Text>버튼!!!!</Text>
-      </TouchableOpacity>
+      style={{
+        backgroundColor: 'white',
+        height: '100%',
+      }}>
+      <Text>Swipe down to close</Text>
     </View>
   );
-};
-const AddTask = () => {
-  return <Text>AddTask</Text>;
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: 'yellow',
+        flex: 1,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <BottomSheet
+        ref={sheetRef}
+        initialSnap={2}
+        snapPoints={['85%', '10%', '10%']}
+        borderRadius={10}
+        renderContent={renderContent}
+      />
+    </SafeAreaView>
+  );
 };
