6d6cc4da971928b08908ca02214b1cd329d42c12
leejiwonn <bbongwa@naver.com>
2021-07-13 21:36:52 +0900

    fix: 네비게이션 타입 변경

--- src/navigations/types.tsx
+++ src/navigations/types.tsx
@@ -2,8 +2,8 @@ import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 export type RootStackParamList = {
-  Home: undefined;
-  AddTask: undefined;
+  Home: React.FC;
+  AddTask: React.FC;
 };
 
 export interface HomeScreenProps {
