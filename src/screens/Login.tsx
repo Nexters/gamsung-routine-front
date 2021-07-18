27532c9296e618d7368e254174861fd4fa77d9c4
leejiwonn <bbongwa@naver.com>
2021-07-18 20:04:04 +0900

    fix: 로그인 뷰 UI 변경

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -7,6 +7,8 @@ import CustomText from '~/components/CustomText';
 import { User } from '~/models/User';
 import { RootStackParamList } from '~/navigations/types';
 import AuthStore from '~/stores/AuthStore';
+import { TextColor } from '~/utils/color';
+import { FontType, Align } from '~/utils/font';
 
 export interface HomeScreenProps {
   navigation: StackNavigationProp<RootStackParamList>;
@@ -31,8 +33,12 @@ const Login = ({ navigation }: HomeScreenProps) => {
 
   return (
     <LoginStyled>
+      <OnBoardingView />
       <KakaoLoginButton onPress={onLogin}>
-        <CustomText>카카오 로그인</CustomText>
+        <KakaoLoginIcon source={require('~/assets/icons/icon_kakao_login.png')} />
+        <CustomText font={FontType.BOLD_LARGE} color={TextColor.PRIMARY} align={Align.CENTER}>
+          카카오 로그인 하기
+        </CustomText>
       </KakaoLoginButton>
     </LoginStyled>
   );
@@ -40,15 +46,35 @@ const Login = ({ navigation }: HomeScreenProps) => {
 
 const LoginStyled = styled.SafeAreaView`
   flex: 1;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   background-color: #fff;
 `;
 
+const OnBoardingView = styled.View`
+  width: 68%;
+  height: 250px;
+  margin-top: 80px;
+  background-color: #f2f2f4;
+  border-radius: 40px;
+`;
+
 const KakaoLoginButton = styled.TouchableOpacity`
-  background-color: #f3f3f3;
-  padding: 15px 30px;
-  border-radius: 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 88%;
+  background-color: #ffca42;
+  padding: 12px 0;
+  margin-bottom: 80px;
+  border-radius: 8px;
+`;
+
+const KakaoLoginIcon = styled.Image`
+  width: 17px;
+  height: 16px;
+  margin-right: 8px;
 `;
 
 export default Login;
