06b54d50e09c9c4ed1abd3b83189ff89ce40cd64
HunJin Kim <ysg01129@naver.com>
2021-07-19 23:13:01 +0900

    fix #22. 리뷰 반영

--- src/screens/AddTask.tsx
+++ src/screens/AddTask.tsx
@@ -1,33 +1,14 @@
 import styled from '@emotion/native';
 import React, { useState } from 'react';
-import BottomSheet from 'reanimated-bottom-sheet';
 
 import AddTaskItem from '~/components/AddTaskItem';
-import BottomSheetContent from '~/components/BottomSheet';
-import { Task } from '~/models/Task';
 import { observer } from 'mobx-react';
 import { BackgroundColor } from '~/utils/color';
 import { SelectCategoryWithTemplate } from '~/components/SelectCategoryWithTemplate';
 import { AddTaskVM } from '~/screens/vm/addTaskVM';
 
 const AddTask = observer(() => {
-  const sheetRef = React.useRef(null);
   const [vm] = useState<AddTaskVM>(new AddTaskVM());
-  const [selectedTasks, setSelectedTasks] = useState<Task[]>([]);
-
-  const onPress = (selectedTask: Task) => {
-    setSelectedTasks((oldSelectedTasks) => {
-      const has = oldSelectedTasks.some((task) => {
-        return task.id === selectedTask.id;
-      });
-      if (has) {
-        return oldSelectedTasks.filter((task) => {
-          return task.id !== selectedTask.id;
-        });
-      }
-      return [...oldSelectedTasks, selectedTask];
-    });
-  };
 
   return (
     <AddTaskStyled>
@@ -47,18 +28,6 @@ const AddTask = observer(() => {
               );
             })}
         </ContentScrollView>
-      )}
-
-      {/* 이제 이 페이지에서 사용하지 않는 컴포넌트인데, 혹시 다른 페이지에서 카피해 갈 때 참고용으로 쓰기 위해 임시로 남겨둠 */}
-      {false && (
-        <BottomSheet
-          ref={sheetRef}
-          initialSnap={2}
-          snapPoints={['85%', '15%', '15%']}
-          borderRadius={8}
-          renderContent={() => <BottomSheetContent selectedTasks={selectedTasks} onPress={onPress} />}
-          enabledInnerScrolling
-        />
       )}
     </AddTaskStyled>
   );
