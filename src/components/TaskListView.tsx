9ee8fc43102fd9a0f4645f9e612b7cf9edbc0f2b
Yuni-Q <lyh6425@gmail.com>
2021-07-27 02:10:10 +0900

    fix: 루틴 없을 때 UI 변경
    fixed #52

--- src/components/TaskListView.tsx
+++ src/components/TaskListView.tsx
@@ -2,11 +2,12 @@ import styled from '@emotion/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React from 'react';
 
-import CustomText from '~/components/CustomText';
+import CustomText from './CustomText';
+
 import TaskListItem from '~/components/TaskListItem';
 import { Task } from '~/models/Task';
 import { RootStackParamList } from '~/navigations/types';
-import { FontType, Align } from '~/utils/font';
+import { Align, FontType } from '~/utils/font';
 
 interface Props {
   navigation: StackNavigationProp<RootStackParamList>;
@@ -22,60 +23,55 @@ const TaskListView = ({ taskList, onToggleTask, isVisiblePopup, onPopupClick, na
   };
 
   return (
-    <TaskListViewStyled>
-      {taskList.length > 0 ? (
-        taskList.map((task, index) => {
-          return (
-            <TaskListItem
-              navigation={navigation}
-              layerIndex={index}
-              totalCount={taskList?.length}
-              key={index}
-              id={task.id}
-              title={task.title}
-              timesOfWeek={task.timesOfWeek}
-              timesOfDay={task.timesOfDay}
-              percent={task.percent}
-              todayOfWeek={task.todayOfWeek}
-              dayOfWeek={task.dayOfWeek}
-              share
-              shareCount={3}
-              shareFinishedCount={1}
-              sharePeople={['1', '2', '3']}
-              sharePercent={30}
-              onTaskItemClick={(id) => onToggleTask(id)}
-              isVisiblePopup={isVisiblePopup}
-              onMoreButtonClick={handleMoreButtonClick}
-            />
-          );
-        })
-      ) : (
+    <>
+      {taskList.length > 0 && (
+        <TaskListViewStyled>
+          {taskList.map((task, index) => {
+            return (
+              <TaskListItem
+                navigation={navigation}
+                layerIndex={index}
+                totalCount={taskList?.length}
+                key={index}
+                id={task.id}
+                title={task.title}
+                timesOfWeek={task.timesOfWeek}
+                timesOfDay={task.timesOfDay}
+                percent={task.percent}
+                todayOfWeek={task.todayOfWeek}
+                dayOfWeek={task.dayOfWeek}
+                share
+                shareCount={3}
+                shareFinishedCount={1}
+                sharePeople={['1', '2', '3']}
+                sharePercent={30}
+                onTaskItemClick={(id) => onToggleTask(id)}
+                isVisiblePopup={isVisiblePopup}
+                onMoreButtonClick={handleMoreButtonClick}
+              />
+            );
+          })}
+        </TaskListViewStyled>
+      )}
+      {!!taskList.length && (
         <EmptyView>
-          <EmptyImage source={require('~/assets/images/empty_monster.png')} />
-          <CustomText font={FontType.BOLD_TITLE_02} align={Align.CENTER}>
-            루틴이 없어요.{'\n'}루틴을 추가해요.
+          <CustomText font={FontType.BOLD_LARGE} align={Align.CENTER}>
+            루틴이 없어요{'\n'}테스크를 추가해보세요.
           </CustomText>
         </EmptyView>
       )}
-    </TaskListViewStyled>
+    </>
   );
 };
 
 const TaskListViewStyled = styled.ScrollView`
   margin-top: 20px;
-  flex: 1;
 `;
 
 const EmptyView = styled.View`
   justify-content: center;
   align-items: center;
-  padding-top: 100px;
-`;
-
-const EmptyImage = styled.Image`
-  width: 173px;
-  height: 98px;
-  margin-bottom: 30px;
+  flex: 1;
 `;
 
 export default TaskListView;
