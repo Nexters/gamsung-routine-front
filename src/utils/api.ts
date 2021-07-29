0f4d177f8910864e3f13ce0eeec10218c57b43f3
leejiwonn <bbongwa@naver.com>
2021-07-29 20:38:36 +0900

    fix: 카카오 로그인 연동

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,31 +1,52 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import axios from 'axios';
+
+const getCommonHeaders = async () => {
+  const token = await AsyncStorage.getItem('token');
+
+  if (token == null) {
+    return undefined;
+  }
+
+  return {
+    Authorization: `Bearer ${JSON.parse(token)}`,
+  };
+};
 
 // TODO : data, opt type 지정
 class API {
   // XXX : _ prefix를 계속 사용할지 고민
   private static _instance: API;
   call = axios.create({
-    baseURL: '',
+    baseURL: 'https://gamsung-routine.herokuapp.com/api/v1',
   });
 
-  public async get<T = any>(url: string, data?: any, opt?: any): Promise<T> {
-    // TODO : date 값을 쿼리스트링으로 만들기
-    return this.call.post(url, opt);
+  public async get<T = any>(url: string, data?: any) {
+    return this.call.get<T>(url, {
+      params: data,
+      headers: await getCommonHeaders(),
+    });
   }
 
-  public async post<T = any>(url: string, data?: any, opt?: any): Promise<T> {
-    return this.call.post(url, data, opt);
+  public async post<T = any>(url: string, data?: any) {
+    return this.call.post<T>(url, data, {
+      headers: await getCommonHeaders(),
+    });
   }
 
-  public async put<T = any>(url: string, data?: any, opt?: any): Promise<T> {
-    return this.call.put(url, data, opt);
+  public async put<T = any>(url: string, data?: any) {
+    return this.call.put<T>(url, data, {
+      headers: await getCommonHeaders(),
+    });
   }
 
-  public async delete<T = any>(url: string, opt?: any): Promise<T> {
-    return this.call.delete(url, opt);
+  public async delete<T = any>(url: string) {
+    return this.call.delete<T>(url, {
+      headers: await getCommonHeaders(),
+    });
   }
 
-  public static notSupported(): any {
+  public static notSupported(): never {
     throw new Error('지원하지 않는 기능입니다');
   }
 
